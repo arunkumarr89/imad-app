eba8df7c34cf248c6a87215ec014912bd9ccc685
Arun Kumar Rajasekaran <arunkumarr89@users.noreply.github.com>
2017-09-05 09:08:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function (req, res) {
